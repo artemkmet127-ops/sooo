00ad685c79c656da73091c52e65e04d70d92c6a4
student <student@SCH-A46PC13>
2025-09-30 16:26:17 +0300

    new imrovements

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -7,7 +7,14 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        static List<Product> products = new()
+        {
+            new Product { Id = 1, Price = 100, Title = "Product 1", Category = "Category A", Quantity = "10" },
+            new Product { Id = 2, Price = 200, Title = "Product 2", Category = "Category B", Quantity = "5" },
+            new Product { Id = 3, Price = 150, Title = "Product 3", Category = "Category A", Quantity = "8" },
+            new Product { Id = 4, Price = 300, Title = "Product 4", Category = "Category C", Quantity = "12" },
+            new Product { Id = 5, Price = 250, Title = "Product 5", Category = "Category B", Quantity = "7" }
+        };
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -31,6 +38,11 @@ namespace WebApplication1.Controllers
         public IActionResult AdminPanel()
         {
             return View();
+        }
+        public IActionResult Delete(int id)
+        {
+
+            return RedirectToAction("AdminPanel");
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Quantity { get; set; }
+    }
+}
