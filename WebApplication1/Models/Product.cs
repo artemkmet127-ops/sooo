c9ff37118b51c7a83e2ea155272d90642ea8e422
student <student@SCH-A46PC13>
2025-10-07 16:29:57 +0300

    w64ywr6

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -3,9 +3,9 @@
     public class Product
     {
         public int Id { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public string Title { get; set; }
-        public string Category { get; set; }
-        public string Quantity { get; set; }
+        public string Model  { get; set; }
+        public int Feedbacks { get; set; }
     }
 }
